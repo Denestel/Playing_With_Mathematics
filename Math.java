7194bbd5274a7905941d3bcd823e8bd5e6691911
Denestel <denestel@gmail.com>
2022-01-10 04:33:53 -0600

    new additions

--- Math.java
+++ Math.java
@@ -3,6 +3,28 @@ import java.util.*;
 public class Math {
     public static void main (String[args]) {
 
+      Scanner input = new Scanner(System.in);
+      int[] numSet = 2;
+
+      System.out.println("What operation are you performing?");
+      Sytem.out.println("+, -, /, *, ^");
+      String op = input.next();
+
+      switch (op) {
+
+        case "+":
+                setNumSet();
+
+      }
+
+    }
+    public int[] getNumSet{
+      return numSet;
+    }
+    public static setNumSet {
+      System.out.println("What are your two numbers?");
+      numSet[0] = input.nextInt();
+      numSet[1] =input.nextInt();
     }
     public int add(int num1, int num2) {
         return num1+num2;
