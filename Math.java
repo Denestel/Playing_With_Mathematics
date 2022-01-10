9554e7bcfab4323c62358c43d6824a293c9ff29d
Denestel <denestel@gmail.com>
2022-01-10 03:31:05 -0600

    Initial commit

---
+++ Math.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Math {
+    public static void main (String[args]) {
+
+    }
+    public int add(int num1, int num2) {
+        return num1+num2;
+    }
+    public int subtr(int num1, int num2) {
+        return num1-num2;
+    }
+    public int divi(int num1, int num2) {
+        return num1/num2
+    }
+    public int multi(int num1, int num2) {
+        return num1*num2;
+    }
+}
